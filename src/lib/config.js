5d2a586494f021743e5dac743704de63475f45b9
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-07-18 02:19:33 +0900

    Fixes tes codes

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,22 +1,11 @@
-/*
 module.exports = {
   url: "http://example.com",
+  // url: "",
   envs: ["dev", "stage", "production"],
   endpoint: {
     "postScore": "/api/v1/postScore",
     "getScore": "/api/v1/getScore",
     "updateAchievement": "/api/v1/updateAchievement",
     "getAchievement": "/api/v1/getAchievement"
-  }
-};
-*/
-module.exports = {
-  url: "http://localhost:8000/test",
-  envs: ["dev", "stage", "production"],
-  endpoint: {
-    "postScore": "/fixture/postScore.json",
-    "getScore": "/fixture/getScore.json",
-    "updateAchievement": "/fixture/updateAchievement.json",
-    "getAchievement": "/fixture/getAchievement.json"
   }
 };
--- test/index.js
+++ test/index.js
@@ -1,36 +1,78 @@
-var b = new bm(123, "dev");
-b.scoreGet(
-  function (data) {
-    console.log(data);
-  }, function (e) {
-    throw new Error(e);
-  }
-);
+describe('testing bm.js', function() {
+  var b;
+  var head = {'Content-Type': 'text/json'};
+  var userId = 1000;
+  before(function(done) {
+    b = new bm(userId, "dev");
+    done();
+  });
+  beforeEach(function() {
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    this.requests = [];
+    this.xhr.onCreate = function(xhr) {
+      this.requests.push(xhr);
+    }.bind(this);
+  });
+  afterEach(function() {
+    this.xhr.restore();
+    this.requests = [];
+  });
 
-b.scorePost(
-  1000,
-  function (data) {
-    console.log(data);
-  }, function (e) {
-    throw new Error(e);
-  }
-);
+  it('have to get score data', function (done) {
+    var data = {
+      score: 100, timestamp: 201507181513
+    };
+    var body = JSON.stringify(data);
+    b.scoreGet(function (data) {
+      assert(typeof data.score, "number");
+      assert(typeof data.timestamp, "number");
+      done();
+    }, function (e) {});
+    this.requests[0].respond(200, head, body);
+  });
 
-b.achievementGet(
-  1000,
-  function (data) {
-    console.log(data);
-  }, function (e) {
-    throw new Error(e);
-  }
-);
+  it('have to post score data', function (done) {
+    var data = {
+      "message": "Updated user’s score",
+      "status": true
+    };
+    var body = JSON.stringify(data);
+    b.scoreGet(function (data) {
+      assert(typeof data.message, "string");
+      assert(typeof data.status, "string");
+      done();
+    }, function (e) {});
+    this.requests[0].respond(200, head, body);
+  });
 
-b.achievementUpdate(
-  1000,
-  1000,
-  function (data) {
-    console.log(data);
-  }, function (e) {
-    throw new Error(e);
-  }
-);
+  it('have to get achievement data', function (done) {
+    var achievementId = 100;
+    var data = {
+      "stepsComplete": 10,
+      "stepsTotal": 10
+    };
+    var body = JSON.stringify(data);
+    b.achievementGet(achievementId, function (data) {
+      assert(typeof data.stepsComplete, "number");
+      assert(typeof data.stepsTotal, "number");
+      done();
+    }, function (e) {});
+    this.requests[0].respond(200, head, body);
+  });
+
+  it('have to update achievement data', function (done) {
+    var achievementId = 100;
+    var stepsComplete = 100;
+    var data = {
+      "message": "Updated user’s achievement",
+      "status": true
+    };
+    var body = JSON.stringify(data);
+    b.achievementUpdate(achievementId, stepsComplete, function (data) {
+      assert(typeof data.message, "string");
+      assert(typeof data.status, "string");
+      done();
+    }, function (e) {});
+    this.requests[0].respond(200, head, body);
+  });
+});
